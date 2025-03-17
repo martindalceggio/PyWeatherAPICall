1fc0730529502f0c81f0eb974b6fc1aedf8d3675
Martin Dalceggio <mdalceggio@andromaco.com.ar>
2025-03-17 10:01:56 -0300

    Version solo currente, con error CORS en iconos

--- app.py
+++ app.py
@@ -8,16 +8,10 @@ app = Flask(__name__)
 api_endpoint = "http://api.weatherbit.io/v2.0/"
 api_key = "7993abaeb9a0423f931813215ee14f2a"  # Replace with your actual API key
 
+#Version solo current weather
 # Function to get current weather data
 def get_current_weather(city):
     url = f"{api_endpoint}current?city={city}&key={api_key}"
-    response = requests.get(url)
-    data = response.json()
-    return data
-
-# Function to get forecast weather data
-def get_forecast_weather(city):
-    url = f"{api_endpoint}forecast/daily?city={city}&key={api_key}"
     response = requests.get(url)
     data = response.json()
     return data
@@ -27,15 +21,13 @@ def get_forecast_weather(city):
 def index():
     city = "La Plata"  # Default city
     current_weather = get_current_weather(city)
-    forecast_weather = get_forecast_weather(city)
-    return render_template("index.html", current_weather=current_weather, forecast_weather=forecast_weather)
+    return render_template("index.html", current_weather=current_weather)
 
 # Route for city search
 @app.route("/city/<city>")
 def city(city):
     current_weather = get_current_weather(city)
-    forecast_weather = get_forecast_weather(city)
-    return render_template("index.html", current_weather=current_weather, forecast_weather=forecast_weather)
+    return render_template("index.html", current_weather=current_weather)
 
 if __name__ == "__main__":
     app.run(debug=True)
